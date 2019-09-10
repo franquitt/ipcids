0cd4e06f82d4222132c058bacbc95fc1292845ac
franco <franco@madTeam>
2019-09-10 11:54:34 -0400

    agregado proyecto

--- cidsipApp/models.py
+++ cidsipApp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+class Proyecto(models.Model):
+	nombre = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.nombre
+
 
 class Computadora(models.Model):
 	nombre = models.CharField(max_length=50)
 	mac = models.CharField(max_length=50)
 	ip = models.CharField(max_length=50)
+	proyecto = models.ForeignKey(Proyecto, on_delete = models.CASCADE, blank = True)
+
+	def __str__(self):
+		return self.nombre
